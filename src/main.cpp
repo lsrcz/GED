30fc3d0b02de33abcd68d0bebb376e9279377078
Evan Wu <evanwu@pku.edu.cn>
2018-06-07 19:39:55 +0800

    搞了几个变量

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
 #include "input.hpp"
+#include <omp.h>
 using namespace std;
 #define MAX_NODE 75
+#define MAX_DSIZE 150*150
+#define MAX_COST 1000000000
+int cvd, ced, cvi, cei, cvs, ces;
 char filename1[]="g1.gxl", filename2[]="g2.gxl";
 int mat1[MAX_NODE][MAX_NODE], mat2[MAX_NODE][MAX_NODE];
 int node1[MAX_NODE], node2[MAX_NODE];
 int node_cnt1, node_cnt2;
+int mat_delta[MAX_DSIZE][MAX_DSIZE];
+
+void get_mat_delta(){
+	memset(mat_delta, 0, sizeof(mat_delta));
+	int dsize=node_cnt1+node_cnt2;
+	#pragma omp parallel for
+	for (int i=0; i<dsize; i++)
+		for (int j=0; j<dsize; j++)
+		{
+			int x=i*(node_cnt1+node_cnt2), y=j*(node_cnt1+node_cnt2);
+			for (int k=0; k<dsize; x++, k++)
+				for (int l=0; l<dsize; y++, l++){
+					if (i<node_cnt1&&j<node_cnt1)
+				}
+		}
+
+}
 
 int main()
 {
 	initmat(filename1, mat1, node1, node_cnt1, filename2, mat2, node2, node_cnt2);
-	printf("%d %d\n", node_cnt1, node_cnt2);
-	for(int i=0; i<node_cnt1; i++)
-		printf("%d ", node1[i]);
-	printf("\n");
-	for(int i=0; i<node_cnt1; i++){
-		for(int j=0; j<node_cnt1; j++)
-			printf("%d", mat1[i][j]);
-		printf("\n");
-	}
 
-	for(int i=0; i<node_cnt2; i++)
-		printf("%d ", node2[i]);
-	printf("\n");
-	for(int i=0; i<node_cnt2; i++){
-		for(int j=0; j<node_cnt2; j++)
-			printf("%d", mat2[i][j]);
-		printf("\n");
-	}
+
 
 }
