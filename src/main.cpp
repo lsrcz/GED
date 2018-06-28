734bf9735e1268419b56dfe0fe2c642b0135f85d
Evan Wu <evanwu@pku.edu.cn>
2018-06-28 21:19:48 +0800

    Auto stash before merge of "master" and "origin/master"
    modify

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "input.hpp"
 #include "genCost.hpp"
+#include "quadratic"
+#include "lsape.hpp"
 //#include <omp.h>
 #include <cstring>
+#include <ctime>
+#include <algorithm>
 #define MAX_NODE 75
 #define MAX_DSIZE 5600
 int cvd=4, ced=2, cvs=2, ces=1;
@@ -18,4 +22,43 @@ int main()
     //g1.printchem(), g2.printchem();
     //delta.printCost();
     //delta.printDelta();
+    srand(time(NULL));
+    double* x = (double*)calloc(delta.d_n, sizeof(double));
+
+    for (int i=0; i<std::min(delta.c_n, delta.c_m); i++)
+    	x[i*delta.c_m+i]=1;
+   	if (delta.c_n>delta.c_m){
+   		for (int i=delta.c_m; i<delta.c_n; i++)
+   			x[(i+1)*delta.c_m-1]=1;
+   	}
+   	else{
+   		for(int i=delta.c_n; i<delta.c_m; i++)
+   			x[(delta.c_n-1)*delta.c_m+i]=1;
+   	}
+   	x[delta.c_n*delta.c_m-1]=1;
+   	for (int i=0; i<delta.c_n; i++){
+   		for(int j=0; j<delta.c_m; j++)
+   			printf("%d ", x[i*delta.c_m+j]);
+   		printf("\n");
+   	}
+    //IPFPmin(x, delta);
+    
+}
+
+void IPFPmin(double* x, const costMat& delta){
+    double* mult_x_delta = new double[delta.d_n];
+    double* mat_cost = new double[delta.d_n];
+    double* b = new double[delta.d_n];
+    vecMulMat(x, delta.mat_delta, mult_x_delta, delta.d_n, delta.d_n);
+    matAdd(mult_x_delta, delta.cost, delta.d_n, 1, mat_cost);
+
+    double s_k = dot(mat_cost, x, delta.d_n);
+    double l = dot(delta.cost, x, delta.d_n);
+
+    solveLSAPE(mat_cost, delta.c_n-1, delta.c_m-1, b);
+    solveQuadratic(delta, x, b, s_k, l, mult_x_delta);
+
+    delete[] mult_x_delta;
+    delete[] mat_cost;
+    delete[] b;
 }
--- src/quadratic.cpp
+++ src/quadratic.cpp
@@ -6,17 +6,18 @@
 //
 //
 
+
 #include "quadratic.hpp"
-#include "string.h"
-#include "stdlib.h"
+#include <cstring>
+#include <cstdlib>
 #include "matop.hpp"
 #include "genCost.hpp"
-void solveQuadratic(costMat c, double* x, double* b, double& s_k, double& l){
+void solveQuadratic(costMat c, double* x, double* b, double& s_k, double& l, double* mult_x_delta){
 	double* mult_delta_b = (double*) calloc(c.d_n, sizeof(double));
 	matMulVec(c.mat_delta, b, mult_delta_b, c.d_n, c.d_n);
 
 	double* mult_x_d = (double*) calloc(c.d_n, sizeof(double));
-	vecMulMat(x, c.mat_d, mult_x_d, c.d_n, c.d_n);
+	scalarMulVec(mult_x_delta, c.d_n, 2, mult_x_d);
 
 	double l_prime = dot(c.cost, b, c.c_m * c.c_n);
 	double s_b = dot(b, mult_delta_b, c.c_m * c.c_n) + l_prime;
@@ -33,8 +34,13 @@ void solveQuadratic(costMat c, double* x, double* b, double& s_k, double& l){
 	}
 	else{
 		double tmp = 1 - t_0;
-		for (int i = 0; i<c.d_n; i++)
-			x[i] = tmp * x[i] + t_0 * b[i];
+		double* tmp_vec1 = (double*) calloc(c.d_n, sizeof(double));
+		double* tmp_vec2 = (double*) calloc(c.d_n, sizeof(double));
+
+		scalarMulVec(b, c.d_n, t_0, tmp_vec1);
+		scalarMulVec(x, c.d_n, tmp, tmp_vec2);
+		matAdd(tmp_vec2, tmp_vec1, c.d_n, 1, x);
+
 		s_k = s_k - alpha * alpha / (4 * beta);
 		l = dot(c.cost, x, c.c_m * c.c_n);
 	}
